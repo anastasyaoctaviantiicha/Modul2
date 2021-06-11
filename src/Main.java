ef2fdac53ca9004e9b250988416fc2e408306334
anastasyaokta <andreiya6@gmail.com>
2021-06-12 03:17:13 +0800

    Enkapsulasi02

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import model.Mahasiswa;
+
+public class Main {
+    public static void main(String[] args) {
+        Mahasiswa m1 = new Mahasiswa("19630975","Anastasya","14-10-2000");
+        m1.tampilkanAtribut();
+        System.out.println(m1.getNama());
+
+    }
+}
